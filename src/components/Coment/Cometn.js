81d87a6199294dbbf703732dbfc6129d02da168b
Hossen <mdmitho199911@gmail.com>
2022-04-04 05:28:34 +0600

    what is contex api done

--- src/components/Coment/Cometn.js
+++ src/components/Coment/Cometn.js
@@ -10,22 +10,30 @@ const Cometn = (props) => {
     return (
      
 
-<div className="">
+
 
 <div className="col">
     <div className="card">
-        <img src={img} className=" img" alt="" />
+        <div className="d-flex">
+      <div className="mt-4 ms-5">
+      <img src={img} className=" img" alt="" />
+      </div>
+      <div className="mt-5 ms-3">
+      <h6><small className='color'>{name}</small></h6>
+      <p><small className='color'>Rating : {rating}</small></p>
+      </div>
+        </div>
         <div className="card-body">
-            <h6><small className='color'>Name</small> : {name}</h6>
+          
             <p><small className='color'>Review</small> : {review}</p>
-            <p><small className='color'>Rating : {rating}</small></p>
+            
         </div>
     </div>
 </div>
 
 
 
-</div>
+
 
       
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLInk/CustomLink'
 import './Header.css'
+
 
 const Header = () => {
     return (
-        <div>
-            <nav >
-                <div className="mt-3 mb-3 text-center ">
-                    <Link className='text-decoration-none menu' to="/home">Home</Link>
-                    <Link className='text-decoration-none menu'to="/reviews">Reviews</Link>
-                    <Link className='text-decoration-none menu' to="/dashboard">Dashboard</Link>
-                    <Link className='text-decoration-none menu' to="/about">About</Link>
-                    <Link className='text-decoration-none menu' to="/blogs">Blogs</Link>
-                </div>
-            </nav>
-        </div>
+  
+ 
+                <nav className="mt-4 mb-3 text-center d-flex justify-content-center">
+                    <CustomLink className='text-decoration-none menu' to="/home">Home</CustomLink>
+                    <CustomLink className='text-decoration-none menu'to="/reviews">Reviews</CustomLink>
+                    <CustomLink className='text-decoration-none menu' to="/dashboard">Dashboard</CustomLink>
+                    <CustomLink className='text-decoration-none menu' to="/about">About</CustomLink>
+                    <CustomLink className='text-decoration-none menu' to="/blogs">Blogs</CustomLink>
+                </nav>
+     
+       
     );
 };
 
